c3e23f7f603abd092fa4405125900503f686a7a1
md96 <mahesh.tella@gmail.com>
2025-06-07 16:44:17 +0530

    initial payment service

---
+++ src/main/java/com/example/payment/PaymentApplication.java
@@ -0,0 +1,30 @@
+package com.example.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/payment")
+
+@SpringBootApplication
+public class PaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApplication.class, args);
+		System.out.println("Inside Payment class ");
+	}
+
+@GetMapping("/{id}")
+public User getMethodName(@PathVariable int id) {
+	System.out.println("Inside Payment  API called ");
+    return new User("Mahveer", id);
+}
+
+
+}
