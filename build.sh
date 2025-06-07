0921a49a951583b2482bf52cf64620b11082ec57
md96 <mahesh.tella@gmail.com>
2025-06-08 01:29:26 +0530

    Initial Java jenkins6

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker build -t payment .
-docker run -d -p 8080:8080 payment
+#docker build -t payment 
+& "C:\Program Files (x86)\Docker\Docker\resources\bin\docker.exe" build -t payment .
+#docker run -d -p 8080:8080 payment
+& "C:\Program Files (x86)\Docker\Docker\resources\bin\docker.exe" docker run -d -p 8080:8080 payment
